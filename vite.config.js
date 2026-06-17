fe8dc382b481b03caf63a9b1c78dd56b02bdd0f7
rishabh <sainirishabh394@gmail.com>
2026-06-17 14:37:32 +0530

    replaced my portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-official-portfolio/'
+  base: '/my-official-portfolio/',
+  base: './'
 })
